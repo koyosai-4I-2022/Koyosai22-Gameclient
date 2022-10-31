ec2b7aab6833862e44d79ece86b5aa1a737c7e4b
hakuyou00 <kurokage.hi@gmail.com>
2022-11-01 08:49:56 +0900

    fix:error

--- KoyosaiBattle/Assets/Scripts/SwordController.cs
+++ KoyosaiBattle/Assets/Scripts/SwordController.cs
@@ -8,17 +8,24 @@ public class SwordController : MonoBehaviour
     [SerializeField]
     StrixReplicator replicator;
 
+    bool isInit = false;
+
     void Start()
     {
-        if(!replicator.isLocal)
+        if(!replicator.isLocal && UIController.instance.playerDataClone != null)
 		{
             this.transform.parent = UIController.instance.playerDataClone.gameObject.transform;
+            isInit = true;
 		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isInit && !replicator.isLocal && UIController.instance.playerDataClone != null)
+        {
+            this.transform.parent = UIController.instance.playerDataClone.gameObject.transform;
+            isInit = true;
+        }
     }
 }

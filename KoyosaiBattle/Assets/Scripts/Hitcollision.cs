9f962392edfe20c9c4f61dee961a240971927207
hakuyou00 <kurokage.hi@gmail.com>
2022-11-02 02:06:20 +0900

    fix:collider sword complete maybe.

--- KoyosaiBattle/Assets/Scripts/Hitcollision.cs
+++ KoyosaiBattle/Assets/Scripts/Hitcollision.cs
@@ -24,6 +24,9 @@ public class Hitcollision : MonoBehaviour
             {
                 // パーティクルシステムのインスタンスを生成する。
                 ParticleSystem newParticle = Instantiate(particle);
+                //　パーティクル発生場所を取得し、その位置に生成する。
+                Vector3 hitPos = collision.ClosestPointOnBounds(this.transform.position);
+                newParticle.transform.position = hitPos;
                 //　パーティクルの発生
                 newParticle.Play();
                 //　インスタンス化したパーティクルの消去
@@ -33,9 +36,6 @@ public class Hitcollision : MonoBehaviour
                 //if (Attack.instance.replicator.isLocal)
                 //return;
                 UIController.instance.playerData.HitPoint -= 3;
-                //　パーティクル発生場所を取得し、その位置に生成する。
-                Vector3 hitPos = collision.ClosestPointOnBounds(this.transform.position);
-                newParticle.transform.position = hitPos;
             }
         } 
     }

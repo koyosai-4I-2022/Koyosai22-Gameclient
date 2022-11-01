291f6d4d3a4d0a54c4deef0c63e4bca2fdb31492
hakuyou00 <kurokage.hi@gmail.com>
2022-11-02 01:59:02 +0900

    fix:collider sword complete maybe

--- KoyosaiBattle/Assets/Scripts/Hitcollision.cs
+++ KoyosaiBattle/Assets/Scripts/Hitcollision.cs
@@ -16,27 +16,26 @@ public class Hitcollision : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
-        if(!replicator.isLocal)
-            return;
-
         if (collision.gameObject.CompareTag("swordcolor002"))
         {
-            string swordName = collision.gameObject.transform.parent.name + "(";
-            string playerName = collision.gameObject.transform.parent.parent.name + "(";
-            if(swordName.Split('(')[1] != playerName.Split('(')[1])
+            var pparent = collision.transform.parent.parent.gameObject;
+            Debug.Log($"{pparent.name}:{collision.name}");
+            if(pparent != this.gameObject)
             {
-                //if (Attack.instance.replicator.isLocal)
-                //return;
-                UIController.instance.playerData.HitPoint -= 2;
                 // パーティクルシステムのインスタンスを生成する。
                 ParticleSystem newParticle = Instantiate(particle);
-                //　パーティクル発生場所を取得し、その位置に生成する。
-                Vector3 hitPos = collision.ClosestPointOnBounds(this.transform.position);
-                newParticle.transform.position = hitPos;
                 //　パーティクルの発生
                 newParticle.Play();
                 //　インスタンス化したパーティクルの消去
-                Destroy(newParticle.gameObject, 1.0f);
+                Destroy(newParticle.gameObject, 1.2f);
+                if(!replicator.isLocal)
+                    return;
+                //if (Attack.instance.replicator.isLocal)
+                //return;
+                UIController.instance.playerData.HitPoint -= 3;
+                //　パーティクル発生場所を取得し、その位置に生成する。
+                Vector3 hitPos = collision.ClosestPointOnBounds(this.transform.position);
+                newParticle.transform.position = hitPos;
             }
         } 
     }

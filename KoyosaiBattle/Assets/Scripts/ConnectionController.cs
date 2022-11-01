7997701019ef47f3ecec56f93f896ee3c59ab00f
hakuyou00 <kurokage.hi@gmail.com>
2022-11-01 23:09:12 +0900

    fix:collision sword

--- KoyosaiBattle/Assets/Scripts/ConnectionController.cs
+++ KoyosaiBattle/Assets/Scripts/ConnectionController.cs
@@ -263,9 +263,9 @@ public class ConnectionController : MonoBehaviour
         panel.SetActive(false);
         //foreach(var mem in StrixNetwork.instance.roomMembers)
         //    Debug.Log($"{mem.Value.GetName()}:{mem.Value.GetUid()}");
-        //UIController.instance.state = UIController.PlayState.Playing;
+        UIController.instance.state = UIController.PlayState.Playing;
 		//UIController.instance.state = UIController.PlayState.Loading;
-		UIController.instance.state = UIController.PlayState.InputSelecting;
+		//UIController.instance.state = UIController.PlayState.InputSelecting;
 		StrixNetwork.instance.selfRoomMember.SetPrimaryKey(-1);
     }
 	// テキストに表示するためのメソッド

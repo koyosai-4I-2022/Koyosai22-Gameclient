a61cfe6dece3bddbbfb60bd9ac8576ccf3796880
hakuyou00 <kurokage.hi@gmail.com>
2022-11-03 21:47:10 +0900

    Eenmy HP Gauge Show

--- KoyosaiBattle/Assets/Scripts/EnemyHPRotate.cs
+++ KoyosaiBattle/Assets/Scripts/EnemyHPRotate.cs
@@ -1,11 +1,27 @@
+using SoftGear.Strix.Unity.Runtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHPRotate : MonoBehaviour
 {
-    // Update is called once per frame
-    void LateUpdate()
+    [SerializeField]
+    StrixReplicator replicator;
+
+	private void Start()
+	{
+		if(replicator.isLocal)
+		{
+			this.gameObject.SetActive(false);
+		}
+		else
+		{
+			this.gameObject.SetActive(true);
+		}
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
     {
         transform.rotation = PlayerMotion.instance.transform.rotation;
     }
